2afed4716c8c7a7be0d8a1b5b3bea9a4864b7777
PetusoTwo <pedrocespedes456@gmail.com>
2024-09-05 13:58:51 -0500

    Errores

--- src/Components/Preguntar.jsx
+++ src/Components/Preguntar.jsx
@@ -1,16 +1,36 @@
 import React, { Fragment, useState } from 'react'
-
-//Definir el state para cantidad
-
-//Definir el state para gastos
-
 export default function Preguntar() {
+
+    const [ cantidad, guardarCantidad ] = useState(0);
+    const [ error, guardarError ] = useState(false);
+
+    //Funcion que lee el presupuesto
+
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value, 10))
+    }
+
+    //Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+        //validar
+        if(cantidad < 1 || isNaN(cantidad)){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+        //cierra el modal
+
+        //Si el presupuesto se ha definido y ppasa la validacion
+    }
   return (
     <Fragment>
       <h2>Ingrese su presupuesto:</h2>
-
-      <form action="">
-        <input type="number" className='u-full-width' placeholder='Presupuesto' />
+    { error ? <p className='alerta-error'>No es un presupuesto valido</p> : null }
+      <form
+      onSubmit = { agregarPresupuesto }
+      >
+        <input type="number" className='u-full-width' placeholder='Presupuesto' onChange={definirPresupuesto}/>
         <input type="submit" className='button-primary u-full-width' value='Definir Presupuesto' />
       </form>
     </Fragment>
